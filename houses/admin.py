2c73824a522384baa7d7a10693bcb927c12ac2a8
judoree <135936771+judoree@users.noreply.github.com>
2024-03-06 17:32:21 +0900

    User admin

--- houses/admin.py
+++ houses/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from .models import House
 
-@admin.register(House) 
+
+@admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
 
-    fields = (
-        "name",
-        "address",
-        ("price_per_night", "pets_allowed"),
-    )
     list_display = (
         "name",
         "price_per_night",
@@ -19,10 +15,4 @@ class HouseAdmin(admin.ModelAdmin):
     list_filter = (
         "price_per_night",
         "pets_allowed",
-        
     )
-    search_fields = (
-        "address",
-    )
-    list_display_links = ("name", "address",)
-    list_editable = ("pets_allowed",)
