d5c184aa3c78d29136c695909169d3014dad9248
judoree <135936771+judoree@users.noreply.github.com>
2024-03-07 18:03:53 +0900

    Uuser default

--- users/models.py
+++ users/models.py
@@ -3,4 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        default="",
+        max_length=150,
+    )
+    is_host = models.BooleanField(default=False)
