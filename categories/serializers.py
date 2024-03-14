30f78eaab982b31388ba5391338afbf35d57a7e5
judoree <135936771+judoree@users.noreply.github.com>
2024-03-14 22:05:58 +0900

    ModelViewSet

--- categories/serializers.py
+++ categories/serializers.py
@@ -7,7 +7,3 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
-
-
-# fields 에는 보이고싶은걸 추가 할수 있다
-# exclude 는 안보이게 하고 싶은걸 추가
