da9d3ed3949ac54c4c1bc0d0d7838ecf78fb61ae
judoree <135936771+judoree@users.noreply.github.com>
2024-03-15 19:07:39 +0900

    Rooms

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.Rooms.as_view()),
     path("amenities/", views.Amenities.as_view()),
     path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
-# 장고 가 제공해주는 url 안에서 변수를 설정 할 수 있음 django 는 그걸view funstion 에 줄거임
