edff55075d779e00da41a390da8455d740ee76a9
judoree <135936771+judoree@users.noreply.github.com>
2024-03-27 16:47:36 +0900

    Custom Resolvers

--- rooms/types.py
+++ rooms/types.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 import strawberry
+from strawberry.types import Info
 from strawberry import auto
 from . import models
+from wishlists.models import Wishlist
 import typing
 from users.types import UserType
 from reviews.types import ReviewType
@@ -17,7 +19,7 @@ class RoomType:
     reviews: typing.List["ReviewType"]
 
     @strawberry.field
-    def reviews(self, page: int) -> typing.List["ReviewType"]:
+    def reviews(self, page: typing.Optional[int] = 1) -> typing.List["ReviewType"]:
         page_size = settings.PAGE_SIZE
         start = (page - 1) * page_size
         end = start + page_size
@@ -26,3 +28,14 @@ class RoomType:
     @strawberry.field
     def rating(self) -> str:
         return self.rating()
+
+    @strawberry.field
+    def is_owner(self, info: Info) -> bool:
+        return self.owner == info.context.request.user
+
+    @strawberry.field
+    def is_liked(self, info: Info) -> bool:
+        return Wishlist.objects.filter(
+            user=info.context.request.user,
+            rooms__pk=self.pk,
+        ).exists()
