40d38563ea2732121fd1452d87a620740fadaf65
judoree <135936771+judoree@users.noreply.github.com>
2024-03-27 15:52:45 +0900

    Type Relationships

--- rooms/types.py
+++ rooms/types.py
@@ -1,6 +1,7 @@
 import strawberry
 from strawberry import auto
 from . import models
+from users.types import UserType
 
 
 @strawberry.django.type(models.Room)
@@ -9,3 +10,4 @@ class RoomType:
     id: auto
     name: auto
     kind: auto
+    owner: "UserType"
