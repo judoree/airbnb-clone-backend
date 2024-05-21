ca81758d57c38081f872456b6418e6955765d6f2
judoree <135936771+judoree@users.noreply.github.com>
2024-05-21 15:39:16 +0900

    Log in

--- users/views.py
+++ users/views.py
@@ -133,7 +133,7 @@ class GithubLogIn(APIView):
         try:
             code = request.data.get("code")
             access_token = requests.post(
-                f"https://github.com/login/oauth/access_token?code={code}&client_id=5195598d392601f20eea&client_secret={settings.GH_SECRET}",
+                f"https://github.com/login/oauth/access_token?code={code}&client_id=Ov23liYKqAM0abtXRi7z&client_secret={settings.GH_SECRET}",
                 headers={"Accept": "application/json"},
             )
             access_token = access_token.json().get("access_token")
